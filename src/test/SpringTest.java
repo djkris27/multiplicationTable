f3c3ac8ba6e6a63a3b667748dd9b6d4dd69cd365
djkris27 <djkrzych@o2.pl>
2023-03-12 22:03:24 +0100

    Add test catalog

---
+++ src/test/SpringTest.java
@@ -0,0 +1,7 @@
+package test;
+
+public class SpringTest {
+    public static void main(String [] args){
+        
+    }
+}
---
+++ src/test/UserData.java
@@ -0,0 +1,70 @@
+package test;
+import java.util.Scanner;
+
+public class UserData {
+    private String name;
+    private int maxNumber, maxResult, quanity;
+
+    UserData(String givenName, int givenMaxNumber, int givenMaxResult, int givenQuantity){
+        name = new String(givenName);
+        maxNumber = givenMaxNumber;
+        maxResult = givenMaxResult;
+        quanity = givenQuantity;
+    }
+
+    UserData(){
+        maxNumber = 0;
+        maxResult = 0;
+        quanity = 0;
+
+        askName();
+        askMaxNumber();
+        askMaxResult();
+        askQuantity();
+    }
+
+    //Dodać wyjątki błędnej nazwy
+    private void askName(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Proszę podać swoje imię: ");
+        name = scan.nextLine();
+    }
+
+    //Dodać wyjątki błędnie wprowadzonej liczby
+    private void askMaxNumber(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Proszę podać maksymalny czynnik iloczynu: ");
+        maxNumber = scan.nextInt();
+    }
+
+    //Dodać wyjątki błędnie wprowadzonej liczby
+    private void askMaxResult(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Proszę podać maksymalny wynik: ");
+        maxResult = scan.nextInt();
+    }
+
+    //Dodać wyjątki błędnie wprowadzonej liczby
+    private void askQuantity(){
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Proszę podać liczbę działań: ");
+        quanity = scan.nextInt();
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getMaxNumber(){
+        return maxNumber;
+    }
+
+    public int getMaxResult(){
+        return maxResult;
+    }
+
+    public int getQuanity(){
+        return quanity;
+    }
+
+}
